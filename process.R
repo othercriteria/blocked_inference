4a169dc1180ae0b74034a26ecefecc88bf49966f
Daniel Klein <othercriteria@gmail.com>
2011-03-29 01:32:49 -0400

    Following changes in hmm.py.

--- process.R
+++ process.R
@@ -1,13 +1,19 @@
 # Generate standard visualization and analyses from data
 
 dat <- read.csv("outfile.csv")
-dat.mix <- dat[dat$model.type == "Mixture",]
-dat.hmm <- dat[dat$model.type == "HMM",]
+dat$blocks <- factor(dat$blocks)
+dat$gamma.init.rep <- factor(dat$gamma.init.rep)
+dat.mix <- dat[dat$shuffled == "True",]
+dat.hmm <- dat[dat$shuffled == "False",]
 
 require(lattice)
 
 pdf("all_run_time_num_data_by_block.pdf")
-with(dat, xyplot(run.time ~ num.data | factor(blocks) + model.type))
+with(dat, xyplot(run.time ~ num.data | blocks + shuffled))
+dev.off()
+
+pdf("all_reps_num_data_by_block.pdf")
+with(dat, xyplot(reps ~ num.data | blocks + shuffled))
 dev.off()
 
 pdf("hmm_run_time_block_by_num_data.pdf")
