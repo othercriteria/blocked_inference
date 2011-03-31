fe5fb15149eee822be193afc3b231da9feee4c18
Daniel Klein <othercriteria@gmail.com>
2011-03-31 18:50:19 -0400

    Initial commit of profiler script.

---
+++ profiler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Experiments with EM estimation on HMM data. Profiling method performance.
+# Daniel Klein, 3/31
+
+import cProfile
+import pstats
+
+from hmm import *
+cProfile.run('main()', 'hmmprof')
+
+p = pstats.Stats('hmmprof')
+p.strip_dirs().sort_stats('cumulative').print_stats(10)
